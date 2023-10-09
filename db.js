74da361cdad327c7e417090b3f44beae79a69fdd
Mohamed149128 <89969395+Mohamed149128@users.noreply.github.com>
2023-10-09 14:35:46 +0200

    first commit

---
+++ db.js
@@ -0,0 +1,390 @@
+
+
+
+//"qs": a string representing the question
+//
+//"options": an array of four strings representing the answer options for the question
+//
+//"answerid": an integer representing the index of the correct answer option in the "options" array for the question
+
+var data= [
+    {
+        "qs": "Q1.Wat is de hoofdstad van Nederland? ",
+        "options": [
+            "Amsterdam",
+            "Edam",
+            "Westwoud",
+            "Hoorn"
+            
+        ],
+        "answerid": 0
+    },
+    {
+        "qs": "Q2. Wat is de hoofdstad van Frankrijk? ",
+        "options": [
+            "Se Arr",
+            "Cro Sa",
+            "Saint-Dennis",
+            "Parijs"
+            
+        ],
+        "answerid": 3
+    },
+
+    {
+        "qs": "Q3. Wat is de hoofdstad van Duitsland? ",
+        "options": [
+            "Hamburg",
+            "Bonn",
+            "Berlijn",
+            "Essen"
+            
+        ],
+        "answerid": 2
+    },
+
+    {
+        "qs": "Q4. Wat is de hoofdstad van de Verenigde Staten? ",
+        "options": [
+            "Washington",
+            "Westwood",
+            "Chicago",
+            "Montana"
+            
+        ],
+        "answerid": 0
+    },
+
+    {
+        "qs": "Q5. Wat is de hoofdstad van de Verenigd Koninkrijk?",
+        "options": [
+            "Liverpool",
+            "London",
+            "Manchester",
+            "Wales"
+            
+        ],
+        "answerid": 1
+    },
+
+    {
+        "qs": "Q6. Wat is de hoofdstad van de Spanje?",
+        "options": [
+            "Sevilla",
+            "Barcalona",
+            "Madrid",
+            "Bilbao"
+            
+        ],
+        "answerid": 2
+    },
+
+    {
+        "qs": "Q7. Wat is de hoofdstad van de Denenmarken?",
+        "options": [
+            "Olested",
+            "Glostrup",
+            "Kopenhagen",
+            "ullpersa"
+            
+        ],
+        "answerid": 2
+    },
+
+    {
+        "qs": "Q8. Wat is de hoofdstad van de België?",
+        "options": [
+            "Antwerpen",
+            "Brugge",
+            "Bergen",
+            "Brussel"
+            
+        ],
+        "answerid": 3
+    },
+
+    {
+        "qs": "Q9. Wat is de hoofdstad van de China?",
+        "options": [
+            "LangFang",
+            "Beijing",
+            "TangShan",
+            "Boading"
+            
+        ],
+        "answerid": 1
+    },
+
+    {
+        "qs": "Q10. Wat is de hoofdstad van de Syrie?",
+        "options": [
+            "Aleppo",
+            "Hasake",
+            "Damascus",
+            "Homs"
+            
+        ],
+        "answerid": 2
+    },
+]
+
+
+
+
+
+
+                                                             //Pokemonnnnn
+
+
+var data2= [
+    {
+        "qs": "Q1.How many evolutions does Eevee have?",
+        "options": [
+            "3",
+            "8",
+            "2",
+            "9"
+            
+        ],
+        "answerid": 1
+    },
+    {
+        "qs": "Q2. Which Pokémon is Misty’s main Pokémon? ",
+        "options": [
+            "Psyduck",
+            "It's subjective and open to interpretation.",
+            "Starmie",
+            "Gyarados"
+            
+        ],
+        "answerid": 2
+    },
+
+    {
+        "qs": "Q3. What are the three types of starter Pokémon? ",
+        "options": [
+            "Electric, Flying, and Rock",
+            "Grass, Fire, and Water",
+            "Psychic, Dark, and Fighting",
+            "There are different types of starter"
+            
+        ],
+        "answerid": 1
+    },
+
+    {
+        "qs": "Q4. How old is Ash in the beginning of the series? ",
+        "options": [
+            "10",
+            "9",
+            "11",
+            "12"
+            
+        ],
+        "answerid": 0
+    },
+
+    {
+        "qs": "Q5. How many members are part of the Squirtle Squad?",
+        "options": [
+            "5",
+            "3",
+            "6",
+            "2"
+            
+        ],
+        "answerid": 0
+    },
+
+    {
+        "qs": "Q6.What is the first Pokémon Ash ever caught?",
+        "options": [
+            "Sabrina",
+            "Misty",
+            "Lt. Surge",
+            "Pikachu"
+            
+        ],
+        "answerid": 3
+    },
+
+    {
+        "qs": "Q7. How many types of Pokémon are there?",
+        "options": [
+            "17",
+            "18",
+            "19",
+            "20"
+            
+        ],
+        "answerid": 1
+    },
+
+    {
+        "qs": "Q8. Who voiced Pikachu in Detective Pikachu?",
+        "options": [
+            "Ryan Reynolds",
+            "Danny DeVito",
+            "Ben Schwartz",
+            "Frank Welker"
+            
+        ],
+        "answerid": 2
+    },
+
+    {
+        "qs": "Q9. Who was the first Pokémon ever designed?",
+        "options": [
+            "Pikachu",
+            "Bulbasaur",
+            "Mew",
+            "Rhydon"
+            
+        ],
+        "answerid": 1
+    },
+
+    {
+        "qs": "Q10. What is Mudkip’s final evolution?",
+        "options": [
+            "Swampert",
+            "Marshtomp",
+            "Quagsire",
+            "Wooper"
+            
+        ],
+        "answerid": 0
+    },
+
+]
+
+
+
+
+
+
+                                                            //Dieren
+
+
+var data3= [
+{
+"qs": "Q1.What is the largest land animal in the world?",
+"options": [
+"Rhino",
+"Elephant",
+"Giraffe",
+"Hippopotamus"
+                                                                        
+],
+"answerid": 1
+},
+{
+"qs": "Q2.What is the fastest animal on land? ",
+"options": [
+"Cheetah",
+"Gazelle.",
+"Wildebeest",
+"Lion"
+                                                                        
+],
+"answerid": 0
+},
+                                                            
+{
+"qs": "Q3.What is the largest species of bear? ",
+"options": [
+"Polar Bear",
+"Grizzly Bear",
+"Panda Bear",
+"Black Bear"
+                                                                        
+],
+"answerid": 0
+},
+                                                            
+{
+"qs": "Q4. What is the most venomous snake in the world? ",
+"options": [
+"Rattlesnake",
+"Cobra",
+"Sea snake",
+"Black mamba"
+                                                                        
+],
+"answerid": 3
+},
+                                                            
+{
+"qs": "Q5.What is the smallest mammal in the world?",
+"options": [
+"Mouse",
+"Bat",
+"Shrew",
+"Hamster"
+                                                                        
+],
+"answerid": 2
+},
+                                                            
+{
+"qs": "Q6.Which bird is known for its ability to mimic human speech?",
+"options": [
+"Raven",
+"Blue Jay",
+"Eagle",
+"Parrot"
+                                                                        
+],
+"answerid": 3
+},
+                                                            
+{
+"qs": "Q7.What is the largest bird in the world?",
+"options": [
+"Ostrich",
+"Pelican",
+"Pelican",
+"Condor"
+                                                                        
+],
+"answerid": 0
+},
+                                                            
+{
+"qs": "Q8.What is the largest mammal in the ocean?",
+"options": [
+"Dolphin",
+"Killer Whale",
+"Blue Whale",
+"Humpback Whale"
+                                                                        
+],
+"answerid": 2
+},
+                                                            
+{
+"qs": "Q9.Which animal is often referred to as the king of the jungle?",
+"options": [
+"Tiger",
+"Lion",
+"Gorilla",
+"Elephant"
+                                                                        
+],
+"answerid": 1
+},
+                                                            
+{
+"qs": "Q10. What is the fastest bird in the world?",
+"options": [
+"Falcon",
+"Eagle",
+"Hummingbird",
+"Ostrich"
+                                                                        
+],
+"answerid": 0
+},
+                                                            
+]
+                                                            
